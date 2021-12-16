89b481ce8f28562f3553521656d3734b49e36318
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-12-16 11:58:02 +0530

    version update

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import Button, { ButtonProps } from '.';
+import Button, { ButtonProps } from './button';
 import styled from 'styled-components';
 import DownloadIcon from '../../icons/Download';
 
