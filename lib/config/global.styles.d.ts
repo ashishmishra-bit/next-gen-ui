0fe8990dbdfc5679acc06e042a4d90ca04188ad9
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-12-17 10:03:14 +0530

    version update

--- lib/config/global.styles.d.ts
+++ lib/config/global.styles.d.ts
@@ -1,3 +1,2 @@
-import './fonts/styles.css';
 export declare const GlobalStyles: import("styled-components").GlobalStyleComponent<{}, import("styled-components").DefaultTheme>;
 //# sourceMappingURL=global.styles.d.ts.map
--- src/config/global.styles.ts
+++ src/config/global.styles.ts
@@ -1,9 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
-import './fonts/styles.css';
+
 
 export const GlobalStyles = createGlobalStyle`
   * {
-    font-family: 'Poppins';
+    
     box-sizing: border-box;
     margin: 0;
     padding: 0;
